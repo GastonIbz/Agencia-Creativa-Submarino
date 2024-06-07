4fa265823889ceb2d94d44e648e6934db76342e7
Gaston ibz <johndoe@example.com>
2024-06-07 16:18:21 -0300

    cambios en posicion de video

--- submarino/src/components/Video.jsx
+++ submarino/src/components/Video.jsx
@@ -15,7 +15,7 @@ const Video = () => {
             />
 
             {/* Contenedor de video centrado con bordes y sombras suaves */}
-            <div className="relative flex justify-center items-center h-full pt-0">
+            <div className="relative flex justify-center items-center h-full pt-20">
                 <video
                     className="w-11/12 md:w-5/6 lg:w-3/4 xl:w-2/3 h-auto  border-4 border-orange-600 rounded shadow-xl transition-all duration-300 ease-in-out"
                     src={submarineVideo}
